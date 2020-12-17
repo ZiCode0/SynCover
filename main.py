c4fae9739b68471feaba1932936bcdd42e9fb772
ZiCode0 <zed.code.05@gmail.com>
2020-12-17 23:43:11 +1200

    :sparkles: add await flag :sparkles:

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ import lib.txt2asc as txt2asc
 
 @logger.catch
 def main():
+    awaiting_flag = True
     logger_lib.init_logger(__program_name__)
     config = importlib.import_module('lib.config')
     logger.info('Program {program_name} started.'.format(program_name=__program_name__))
@@ -23,6 +24,7 @@ def main():
         data_files = files.list_data_folder_files()
         # if files are found
         if len(data_files) != 0:
+            awaiting_flag = True
             logger.info('Reloading config.')
             importlib.reload(config)
             for file in data_files:
@@ -31,6 +33,12 @@ def main():
                 files.move_from_sub_folder(target_folder)
                 txt2asc.run_converter(station.define_station_by_pathname(target_folder), target_folder)
                 files.used_files_cleaner(target_folder)
+            # timer to sleep
+            time.sleep(config.scan_data_folder_in_seconds)
+        else:
+            if awaiting_flag:
+                logger.info('Waiting for incoming files..')
+                awaiting_flag = False
             # timer to sleep
             time.sleep(config.scan_data_folder_in_seconds)
 
